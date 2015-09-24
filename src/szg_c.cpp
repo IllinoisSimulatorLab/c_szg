955f52d2166b7cd7237811ab54407bd31fe614f4
Jim Crowell <jimc@illinois.edu>
2015-09-24 09:46:15 -0500

    Created szg_c.h header, modified .gitignores in build to ignore .h files

--- src/szg_c.cpp
+++ src/szg_c.cpp
@@ -1,12 +1,6 @@
+#include "szg_c.h"
+
 #include <string>
-#include "arSZGClient.h"
-
-#ifdef AR_USE_WIN_32
-#define EXPORT_API __declspec(dllexport)
-#else
-#define EXPORT_API
-#endif
-
 
 std::string _bufString;
 
@@ -14,34 +8,34 @@ char* argvG[] = {"unity"};
 int argcG = 1;
 
 
-extern "C" {
-    arSZGClient* EXPORT_API szgClient() {
-        return new arSZGClient();
-    }
+arSZGClient* szgClient() {
+    return new arSZGClient();
+}
 
-    int EXPORT_API init_szgClient( arSZGClient* cli, char* forcedName ) {
-        int retVal = -1;
-        int argc = argcG;
-        char** argvPtr = new char*[argc];
-        for (int i=0; i<argc; ++i) {
-            argvPtr[i] = new char[strlen(argvG[i])];
-        }
-        if (cli->init( argc, argvPtr, forcedName )) {
-            retVal = argc;
-        }
-        for (int i=0; i<argc; ++i) {
-            delete[] argvPtr[i];
-        }
-        delete[] argvPtr;
-        return retVal;
+
+int init_szgClient( arSZGClient* cli, char* forcedName ) {
+    int retVal = -1;
+    int argc = argcG;
+    char** argvPtr = new char*[argc];
+    for (int i=0; i<argc; ++i) {
+        argvPtr[i] = new char[strlen(argvG[i])];
     }
-            
-    void EXPORT_API delete_szgClient( arSZGClient* cli ) {
-        if (cli == NULL) {
-            return;
-        }
-        delete cli;
+    if (cli->init( argc, argvPtr, forcedName )) {
+        retVal = argc;
     }
+    for (int i=0; i<argc; ++i) {
+        delete[] argvPtr[i];
+    }
+    delete[] argvPtr;
+    return retVal;
+}
+        
+
+void delete_szgClient( arSZGClient* cli ) {
+    if (cli == NULL) {
+        return;
+    }
+    delete cli;
 }
 
 
---
+++ src/szg_c.h
@@ -0,0 +1,21 @@
+#ifndef SZG_C_H
+#define SZG_C_H
+
+#include "arSZGClient.h"
+
+#ifdef AR_USE_WIN_32
+#define EXPORT_API __declspec(dllexport)
+#else
+#define EXPORT_API
+#endif
+
+
+extern "C" {
+    arSZGClient* EXPORT_API szgClient();
+
+    int EXPORT_API init_szgClient( arSZGClient* cli, char* forcedName );
+            
+    void EXPORT_API delete_szgClient( arSZGClient* cli );
+}
+
+#endif
