02d5418df1188f2f3cfe363818e947ac3bc2aad7
Jim Crowell <jimc@illinois.edu>
2015-09-24 10:14:38 -0500

    Updated python/setup.py to refresh dll when needed & python/.gitignore to ignore generated .cpp file

--- python/setup.py
+++ python/setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import os
+import shutil
 
 
 currDir = os.getcwd()
@@ -11,6 +12,28 @@ rootDir = os.path.dirname( currDir )
 szgIncDir = os.path.join( rootDir, 'include' )
 szgcIncDir = os.path.join( rootDir, 'src' )
 szgLibDir = os.path.join( rootDir, 'lib', sys.platform )
+
+
+# Make sure szg_c.dll is current
+dllPath = os.path.join( currDir, 'szg_c.dll' )
+dllBuildPath = os.path.join( rootDir, 'build', sys.platform, 'szg_c.dll' )
+
+if not os.path.exists( dllPath ):
+    if not os.path.exists( dllBuildPath ):
+        raise OSError, "shared library szg_c.dll does not exist, you need to run 'scons' in the parent directory"
+    else:
+        print( "Copying '{}' to '{}'...".format( dllBuildPath, dllPath ) )
+        shutil.copyfile( dllBuildPath, dllPath )
+
+elif not os.path.exists( dllBuildPath ):
+    print( "WARNING:  shared library '{}' does not exist".format( dllBuildPath ) )
+else:
+    currentDllctime = os.stat( dllPath )
+    buildDllctime = os.stat( dllBuildPath )
+    if currentDllctime < buildDllctime:
+        print( "Copying '{}' to '{}'...".format( dllBuildPath, dllPath ) )
+        shutil.copyfile( dllBuildPath, dllPath )
+
 
 
 szgLibs = ['arDrivers_static','arBarrier_static',
